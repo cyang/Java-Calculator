12dd751add5e56d844ed4c64520792bf17d6a401
Christopher Yang <cyang001@citymail.cuny.edu>
2015-04-23 23:46:13 -0400

    Organize the code

--- src/JavaGUI.java
+++ src/JavaGUI.java
@@ -3,13 +3,21 @@ import java.awt.*;
 import java.awt.event.*;
 
 /**
- * Created by ChrisYang on 4/16/15.
+ * @author      Christopher Yang <cyang001@citymail.cuny.edu>
+ * @version     1.0
+ * @since       2015-04-23
  */
+
+
 public class JavaGUI extends JFrame {
+    // Two operands
     private double number1 = 0;
     private double number2 = 0;
-    private double memory = 0;
+
     private double result = 0;
+    private double memory = 0;
+
+    // State of the current operation
     private String currentOperation = "";
 
     public static void main(String[] args) {
@@ -24,48 +32,37 @@ public class JavaGUI extends JFrame {
 
         Container contentPane = getContentPane();
         contentPane.setLayout(new BorderLayout(5,5));
-        setSize(300, 300);
-
-        JPanel numberButtonPanel = new JPanel();
-        numberButtonPanel.setLayout(new GridLayout(4, 3, 2, 2));
-
-        JPanel operatorPanel = new JPanel();
-        operatorPanel.setLayout(new GridLayout(5,1,2,2));
-
-        JPanel memoryPanel = new JPanel();
-        memoryPanel.setLayout(new GridLayout(4,1,2,2));
-
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
+        setSize(300, 300);
+        setVisible(true);
 
-        final JTextField numField = new JTextField("0");
+        // Groups the numberButtons
+        JPanel numberButtonPanel = new JPanel();
+        numberButtonPanel.setLayout(new GridLayout(4, 3, 1, 1));
+
+        // Groups the operatorButtons
+        JPanel operatorPanel = new JPanel();
+        operatorPanel.setLayout(new GridLayout(5,1,2,2));
+
+        // Groups the memoryButtons
+        JPanel memoryPanel = new JPanel();
+        memoryPanel.setLayout(new GridLayout(4,1,2,2));
+
+        // Textfield to display the numbers
+        final JTextField numField = new JTextField("");
         numField.setHorizontalAlignment(JTextField.RIGHT);
 
+        // Creating the buttons
         final JButton mrButton = new JButton("MR");
         final JButton mcButton = new JButton("MC");
         final JButton mAddButton = new JButton("M+");
         final JButton mSubButton = new JButton("M-");
-
         final JButton clearButton = new JButton("Clear");
-
-        final JButton button1 = new JButton("1");
-        final JButton button2 = new JButton("2");
-        final JButton button3 = new JButton("3");
-        final JButton button4 = new JButton("4");
-        final JButton button5 = new JButton("5");
-        final JButton button6 = new JButton("6");
-        final JButton button7 = new JButton("7");
-        final JButton button8 = new JButton("8");
-        final JButton button9 = new JButton("9");
-        final JButton button0 = new JButton("0");
-
-        final JButton addButton = new JButton("+");
-        final JButton subButton = new JButton("-");
-        final JButton divButton = new JButton("/");
-        final JButton multButton = new JButton("*");
         final JButton equalButton = new JButton("=");
 
+        // Clears the number on the screen
         clearButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -76,6 +73,7 @@ public class JavaGUI extends JFrame {
             }
         });
 
+        // Recalls the memory
         mrButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -84,6 +82,7 @@ public class JavaGUI extends JFrame {
             }
         });
 
+        // Clears the memory
         mcButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -91,6 +90,7 @@ public class JavaGUI extends JFrame {
             }
         });
 
+        // Adds to the memory
         mAddButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -100,6 +100,7 @@ public class JavaGUI extends JFrame {
             }
         });
 
+        // Subtracts from the memory
         mSubButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -109,185 +110,7 @@ public class JavaGUI extends JFrame {
             }
         });
 
-        button1.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e){
-                if(currentOperation.equals("")){
-                    number1 = (number1*10)+1;
-                    numField.setText(Double.toString(number1));
-                }
-                else
-                {
-                    number2 = (number2*10)+1;
-                    numField.setText(Double.toString(number2));
-                }
-
-            }
-        });
-        button2.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e){
-                if(currentOperation.equals("")){
-                    number1 = (number1*10)+2;
-                    numField.setText(Double.toString(number1));
-                }
-                else
-                {
-                    number2 = (number2*10)+2;
-                    numField.setText(Double.toString(number2));
-                }
-            }
-        });
-        button3.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e){
-                if(currentOperation.equals("")){
-                    number1 = (number1*10)+3;
-                    numField.setText(Double.toString(number1));
-                }
-                else
-                {
-                    number2 = (number2*10)+3;
-                    numField.setText(Double.toString(number2));
-                }
-            }
-        });
-        button4.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e){
-                if(currentOperation.equals("")){
-                    number1 = (number1*10)+4;
-                    numField.setText(Double.toString(number1));
-                }
-                else
-                {
-                    number2 = (number2*10)+4;
-                    numField.setText(Double.toString(number2));
-                }
-            }
-        });
-        button5.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e){
-                if(currentOperation.equals("")){
-                    number1 = (number1*10)+5;
-                    numField.setText(Double.toString(number1));
-                }
-                else
-                {
-                    number2 = (number2*10)+5;
-                    numField.setText(Double.toString(number2));
-                }
-            }
-        });
-        button6.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e){
-                if(currentOperation.equals("")){
-                    number1 = (number1*10)+6;
-                    numField.setText(Double.toString(number1));
-                }
-                else
-                {
-                    number2 = (number2*10)+6;
-                    numField.setText(Double.toString(number2));
-                }
-            }
-        });
-        button7.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e){
-                if(currentOperation.equals("")){
-                    number1 = (number1*10)+7;
-                    numField.setText(Double.toString(number1));
-                }
-                else
-                {
-                    number2 = (number2*10)+7;
-                    numField.setText(Double.toString(number2));
-                }
-            }
-        });
-        button8.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e){
-                if(currentOperation.equals("")){
-                    number1 = (number1*10)+8;
-                    numField.setText(Double.toString(number1));
-                }
-                else
-                {
-                    number2 = (number2*10)+8;
-                    numField.setText(Double.toString(number2));
-                }
-            }
-        });
-        button9.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e){
-                if(currentOperation.equals("")){
-                    number1 = (number1*10)+9;
-                    numField.setText(Double.toString(number1));
-                }
-                else
-                {
-                    number2 = (number2*10)+9;
-                    numField.setText(Double.toString(number2));
-                }
-            }
-        });
-        button0.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e){
-                if(currentOperation.equals("")){
-                    number1 *= 10;
-                    numField.setText(Double.toString(number1));
-                }
-                else
-                {
-                    number2 *= 10;
-                    numField.setText(Double.toString(number2));
-                }
-            }
-        });
-
-
-        addButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                number1 = Double.parseDouble(numField.getText());
-                currentOperation = "+";
-                numField.setText("");
-            }
-        });
-
-        subButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                number1 = Double.parseDouble(numField.getText());
-                currentOperation = "-";
-                numField.setText("");
-            }
-        });
-
-        divButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                number1 = Double.parseDouble(numField.getText());
-                currentOperation = "/";
-                numField.setText("");
-            }
-        });
-
-        multButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                number1 = Double.parseDouble(numField.getText());
-                currentOperation = "*";
-                numField.setText("");
-            }
-        });
-
+        // Pressing the equal button after an operator will give the correct solution
         equalButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -312,21 +135,57 @@ public class JavaGUI extends JFrame {
             }
         });
 
-        numberButtonPanel.add(button7);
-        numberButtonPanel.add(button8);
-        numberButtonPanel.add(button9);
-        numberButtonPanel.add(button4);
-        numberButtonPanel.add(button5);
-        numberButtonPanel.add(button6);
-        numberButtonPanel.add(button1);
-        numberButtonPanel.add(button2);
-        numberButtonPanel.add(button3);
-        numberButtonPanel.add(button0);
+        // OperatorListener listens for an action from operator Buttons
+        class OperatorListener implements ActionListener{
+            public void actionPerformed(ActionEvent e){
+                String operator = e.getActionCommand();
+                number1 = Double.parseDouble(numField.getText());
+                currentOperation = operator;
+                numField.setText("");
+            }
+        }
 
-        operatorPanel.add(divButton);
-        operatorPanel.add(multButton);
-        operatorPanel.add(addButton);
-        operatorPanel.add(subButton);
+        // NumberListener listens for an action from the numberButtons
+        class NumberListener implements ActionListener{
+            public void actionPerformed(ActionEvent e){
+                String digit = e.getActionCommand();
+                if(currentOperation.equals("")){
+                    number1 = (number1*10)+Integer.parseInt(digit);
+                    numField.setText(Double.toString(number1));
+                }
+                else
+                {
+                    number2 = (number2*10)+Integer.parseInt(digit);
+                    numField.setText(Double.toString(number2));
+                }
+            }
+        }
+
+        // Using NumberListener
+        ActionListener numberListener = new NumberListener();
+
+        int[] numberList = {7,8,9,4,5,6,1,2,3,0};
+
+        // Go through the numberList and create the buttons and actions for these numbers
+        for (int element: numberList) {
+            JButton numberButton = new JButton(Integer.toString(element));
+            numberButton.addActionListener(numberListener);
+            numberButtonPanel.add(numberButton);
+        }
+
+        // Using OperatorListener
+        ActionListener operatorListener = new OperatorListener();
+
+        char[] operatorList = {'/', '*', '+', '-'};
+
+        // Go through the operatorList and create the buttons and actions for these operators
+        for (char element: operatorList) {
+            JButton operatorButton = new JButton(Character.toString(element));
+            operatorButton.addActionListener(operatorListener);
+            operatorPanel.add(operatorButton);
+        }
+
+
         operatorPanel.add(equalButton);
 
         memoryPanel.add(mrButton);
@@ -334,12 +193,11 @@ public class JavaGUI extends JFrame {
         memoryPanel.add(mAddButton);
         memoryPanel.add(mSubButton);
 
+        // Adjusts the location of the panels
         contentPane.add(numField, BorderLayout.NORTH);
         contentPane.add(numberButtonPanel, BorderLayout.CENTER);
         contentPane.add(operatorPanel, BorderLayout.EAST);
         contentPane.add(memoryPanel, BorderLayout.WEST);
         contentPane.add(clearButton, BorderLayout.SOUTH);
-
-        setVisible(true);
     }
 }
